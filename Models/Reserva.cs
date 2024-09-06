b2cf5937884b29e23a9c777275a9b4ed8e7cb55c
Robson Ferreira <robsonferr@hotmail.com>
2024-09-06 08:18:32 -0300

    Aplica as regras de negócio definidas.

--- Models/Reserva.cs
+++ Models/Reserva.cs
@@ -15,16 +15,20 @@ namespace DesafioProjetoHospedagem.Models
 
         public void CadastrarHospedes(List<Pessoa> hospedes)
         {
-            // TODO: Verificar se a capacidade é maior ou igual ao número de hóspedes sendo recebido
-            // *IMPLEMENTE AQUI*
-            if (true)
+            try
             {
-                Hospedes = hospedes;
+                if (hospedes.Count() <= this.Suite.Capacidade)
+                {
+                    Hospedes = hospedes;
+                }
+                else
+                {
+                    throw new Exception("A suíte escolhida não possuiu capacidade para a quantidade de hóspedes informada");
+                }
             }
-            else
+            catch (Exception ex)
             {
-                // TODO: Retornar uma exception caso a capacidade seja menor que o número de hóspedes recebido
-                // *IMPLEMENTE AQUI*
+                Console.WriteLine(ex.Message);
             }
         }
 
@@ -35,23 +39,17 @@ namespace DesafioProjetoHospedagem.Models
 
         public int ObterQuantidadeHospedes()
         {
-            // TODO: Retorna a quantidade de hóspedes (propriedade Hospedes)
-            // *IMPLEMENTE AQUI*
-            return 0;
+            return this.Hospedes != null ? this.Hospedes.Count() : 0;
         }
 
         public decimal CalcularValorDiaria()
         {
-            // TODO: Retorna o valor da diária
-            // Cálculo: DiasReservados X Suite.ValorDiaria
-            // *IMPLEMENTE AQUI*
-            decimal valor = 0;
+            decimal valor = this.DiasReservados * this.Suite.ValorDiaria;
 
             // Regra: Caso os dias reservados forem maior ou igual a 10, conceder um desconto de 10%
-            // *IMPLEMENTE AQUI*
-            if (true)
+            if (this.DiasReservados >= 10)
             {
-                valor = 0;
+                valor = valor - (valor * 0.10m);;
             }
 
             return valor;
